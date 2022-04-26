7ff3026b9c3dca0040d20232dccc60ea9f1ce4c2
Renthop <44197289+Renth0p@users.noreply.github.com>
2022-04-26 12:35:04 +0300

    Создана шапка для сайта + основные стили

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<div class="wrapper">
 <?php
 session_start();
 
@@ -6,30 +7,31 @@ require "lib/layouts/app.php";
 require "lib/layouts/header.php";
 ?>
 
-    <form action="lib/handler.php" method="post">
-        <input name="phone" type="text" value="+7 (___) ___-__-__" pattern="\+?[0-9\s\-\(\)]+"  required><br>
-        <input name="first_name" type="text" placeholder="Введите имя"><br>
-        <input name="last_name" type="text" placeholder="Введите фамилию"><br>
-        <input name="city" type="text" placeholder="Введите город"><br>
-        <input name="street" type="text" placeholder="ул. "><br>
-        <input name="postal" type="text" placeholder="Введите индекс" pattern="[0-9]{6}"><br>
-        <button type="submit">клик</button>
-    </form>
-    <?php
-    if($_SESSION['user']){
-        echo 'Номер телефона: '. $_SESSION['user']['phone']."<br>";
-        echo 'Фамилия: '. $_SESSION['user']['l_name']."<br>";
-        echo 'Имя: '. $_SESSION['user']['f_name']."<br>";
-    }
-    unset($_SESSION['user']);
+<form action="lib/handler.php" method="post">
+    <input name="phone" type="text" value="+7 (___) ___-__-__" pattern="\+?[0-9\s\-\(\)]+"  required><br>
+    <input name="first_name" type="text" placeholder="Введите имя"><br>
+    <input name="last_name" type="text" placeholder="Введите фамилию"><br>
+    <input name="city" type="text" placeholder="Введите город"><br>
+    <input name="street" type="text" placeholder="ул. "><br>
+    <input name="postal" type="text" placeholder="Введите индекс" pattern="[0-9]{6}"><br>
+    <button type="submit">клик</button>
+</form>
+<?php
+if($_SESSION['user']){
+    echo 'Номер телефона: '. $_SESSION['user']['phone']."<br>";
+    echo 'Фамилия: '. $_SESSION['user']['l_name']."<br>";
+    echo 'Имя: '. $_SESSION['user']['f_name']."<br>";
+}
+unset($_SESSION['user']);
 
-    if($_SESSION['message']){
-        echo '<p id="errorMess"> '. $_SESSION['message'] . '</p>';
-    }
-    unset($_SESSION['message']);
-    ?>
+if($_SESSION['message']){
+    echo '<p id="errorMess"> '. $_SESSION['message'] . '</p>';
+}
+unset($_SESSION['message']);
+?>
 
 
 <?php
 require "lib/layouts/footer.php";
 ?>
+</div>
--- lib/layouts/header.php
+++ lib/layouts/header.php
@@ -1,3 +1,13 @@
     <header>
-        Я шапка
+        <a href="index.php" class="logo">TelDir</a>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Главная</a>
+                    <a href="#">О нас</a>
+                    <a href="#">Политика</a>
+                </li>
+            </ul>
+        </nav>
+        <a href="#" class="header__button">Авторизация</a>
     </header>
