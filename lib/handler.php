ddb8e0cc2a552bad8a5811e8ad6e8a3bc5392c1b
xtryfix <facyoucraft@gmail.com>
2022-04-26 16:24:27 +0300

    Обновлены условия

--- lib/handler.php
+++ lib/handler.php
@@ -4,7 +4,7 @@ require_once 'bd.php';
 $phone = $_POST['phone'];
 $lastname = $_POST['last_name'];
 
-if(strlen($phone) > 1){
+if(strlen($phone) > 1 and strlen($lastname) < 1){
     $check_user = mysqli_query($bd, "SELECT * FROM `users` WHERE `phone` = '$phone'");
     if (mysqli_num_rows($check_user) > 0) {
         $user = mysqli_fetch_all($check_user);
@@ -14,7 +14,7 @@ if(strlen($phone) > 1){
     $_SESSION['message'] = 'Данных не существует или они введенны не верно!';
     header('Location: ../index.php');
 
-} elseif (strlen($lastname) > 1) {
+} elseif (strlen($lastname) > 1 and strlen($phone) < 1) {
     $check_user = mysqli_query($bd, "SELECT * FROM `users` WHERE `l_name` = '$lastname'");
     if (mysqli_num_rows($check_user) > 0) {
         $user = mysqli_fetch_all($check_user);
