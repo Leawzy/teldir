dac67de13769b3ffffbbcf46ae5bfed4b4e55b84
Renthop <44197289+Renth0p@users.noreply.github.com>
2022-04-27 10:56:09 +0300

    Переделана Главная страница

--- index.php
+++ index.php
@@ -18,46 +18,49 @@ require "lib/layouts/header.php";
         </div>
     </div>
     <div class="form__search">
-        <h2>Телефонный справочник</h2>
-        <div class="border"></div>
-        <form action="lib/handler.php" method="post">
-            Введите имя<br>
-            <input name="phone" type="text" placeholder="+70000000000" pattern="\+?[0-9]{11}"><br>
-            или фамилию<br>
-            <input name="last_name" type="text" placeholder="Введите фамилию"><br>
-            <button type="submit">клик</button>
-        </form>
+        <div class="form__search-form">
+            <h2>Телефонный справочник</h2>
+            <div class="border"></div>
+            <form action="lib/handler.php" method="post">
+                Введите имя<br>
+                <input name="phone" type="text" placeholder="+70000000000" pattern="\+?[0-9]{11}"><br>
+                или фамилию<br>
+                <input name="last_name" type="text" placeholder="Введите фамилию"><br>
+                <button type="submit">клик</button>
+            </form>
+        </div>
+        <div class="form__search-table">
+            <?php
+            if($_SESSION['message']){
+                echo '<p class="errorMess" id="errorMess"> '. $_SESSION['message'] . '</p>';
+            }
+            unset($_SESSION['message']);
+
+
+            if($_SESSION['user']) {
+            ?>
+            <table>
+                <tr>
+                    <th>ФИО</th>
+                    <th>Номер телефона</th>
+                    <th>Адрес</th>
+                </tr>
+                <?php
+                foreach ($_SESSION['user'] as $value) {
+                    ?>
+                    <tr>
+                        <td><?=$value[2]. " " .$value[1]. " " .$value[3]?></td>
+                        <td><?=$value[8]?></td>
+                        <td><?="ул. ". $value[5] . " д. " . $value[6] . " кв. " . $value[7] . " Индекс: " . $value[9]?></td>
+                    </tr>
+                    <?php
+                }
+                }
+                unset($_SESSION['user']);
+                ?>
+            </table>
+        </div>
     </div>
-
-<?php
-if($_SESSION['message']){
-    echo '<p id="errorMess"> '. $_SESSION['message'] . '</p>';
-}
-unset($_SESSION['message']);
-
-
-if($_SESSION['user']) {
-    ?>
-    <table>
-    <tr>
-        <th>ФИО</th>
-        <th>Номер телефона</th>
-        <th>Адрес</th>
-    </tr>
-    <?php
-    foreach ($_SESSION['user'] as $value) {
-        ?>
-        <tr>
-            <td><?=$value[2]. " " .$value[1]. " " .$value[3]?></td>
-            <td><?=$value[8]?></td>
-            <td><?="ул. ". $value[5] . " д. " . $value[6] . " кв. " . $value[7] . " Индекс: " . $value[9]?></td>
-        </tr>
-        <?php
-    }
-}
-unset($_SESSION['user']);
-?>
-    </table>
 <?php
 require "lib/layouts/footer.php";
 ?>
